d99e3cbec85784179ced8ae3c533e4828766b658
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-09-01 18:13:50 -0400

    adds attributes to actor and movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   get "/actors" => "actors#index"
   get "/actors/:id" => "actors#show"
-  post "/actors/create" => "actors#create"
+  post "/actors" => "actors#create"
   patch "actors/:id" => "actors#update"
   delete "actors/:id" => "actors#destroy"
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show"
-  post "/movies/create" => "movies#create"
+  post "/movies" => "movies#create"
   patch "movies/:id" => "movies#update"
   delete "movies/:id" => "movies#destroy"
 end
